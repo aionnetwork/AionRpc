2ea2bab2101fd1c156b07f6d41b6a9db852a1916
kelvin <kelvin@aion.network>
2019-08-18 01:31:13 -0400

    Minor refactor

--- src/main/java/org/aion/api/schema/RootTypes.java
+++ src/main/java/org/aion/api/schema/RootTypes.java
@@ -9,22 +9,20 @@ import org.aion.api.serialization.SerializationUtils;
  * The 'root' types of RPC Autogeneration framework.
  *
  * These correspond to the types in resources/schemas/type/root.json.
- *
  */
 public class RootTypes {
-    private static final ObjectMapper om = new ObjectMapper();
-
     public static final NamedRpcType DATA;
     public static final NamedRpcType QUANTITY;
     public static final NamedRpcType BOOLEAN;
     public static final NamedRpcType OBJECT;
 
     static {
+        final ObjectMapper om = new ObjectMapper();
         try {
             DATA = new NamedRpcType(
                 "DATA",
                 SerializationUtils.loadSchema(om,
-                    new JsonSchemaRef("derived.json#/definitions/DATA")),
+                    new JsonSchemaRef("root.json#/definitions/DATA")),
                 null,
                 null,
                 List.of(),
@@ -33,7 +31,7 @@ public class RootTypes {
             QUANTITY = new NamedRpcType(
                 "QUANTITY",
                 SerializationUtils.loadSchema(om,
-                    new JsonSchemaRef("derived.json#/definitions/QUANTITY")),
+                    new JsonSchemaRef("root.json#/definitions/QUANTITY")),
                 null,
                 null,
                 List.of(),
@@ -42,7 +40,7 @@ public class RootTypes {
             BOOLEAN = new NamedRpcType(
                 "BOOLEAN",
                 SerializationUtils.loadSchema(om,
-                    new JsonSchemaRef("derived.json#/definitions/BOOLEAN")),
+                    new JsonSchemaRef("root.json#/definitions/BOOLEAN")),
                 null,
                 null,
                 List.of(),
@@ -51,16 +49,14 @@ public class RootTypes {
             OBJECT = new NamedRpcType(
                 "OBJECT",
                 SerializationUtils.loadSchema(om,
-                    new JsonSchemaRef("derived.json#/definitions/OBJECT")),
+                    new JsonSchemaRef("root.json#/definitions/OBJECT")),
                 null,
                 null,
                 List.of(),
                 "java.lang.Object"
             );
         } catch (IOException ioe) {
-            // TODO This seems like a terrible idea
-            throw new RuntimeException(
-                "Initialization error: Can't load schemas of base types.", ioe);
+            throw new ExceptionInInitializerError(ioe);
         }
     }
 }

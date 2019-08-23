c4ed26b026c76f02688a4246248ab8d6a6a279d9
aion-kelvin <kelvin@aion.network>
2019-08-23 14:04:43 -0400

    Remove prototype class; formatting of Rpc.java template

--- src/main/java/org/aion/api/prototype/UnauthorizedException.java
+++
@@ -1,8 +0,0 @@
-package org.aion.api.prototype;
-
-public class UnauthorizedException extends org.aion.api.RpcException {
-
-    public UnauthorizedException(String data) {
-        super(10001, "Unauthorized", data);
-    }
-}
